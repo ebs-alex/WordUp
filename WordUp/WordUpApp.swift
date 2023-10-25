1c4f010d893ba5d7a381e4efd0909530eac54f6b
Alex Moran <ebs.alex@icloud.com>
2023-10-25 15:46:38 -0700

    Initial Commit

---
+++ WordUp/WordUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordUpApp.swift
+//  WordUp
+//
+//  Created by Alex Moran on 10/25/23.
+//
+
+import SwiftUI
+
+@main
+struct WordUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
